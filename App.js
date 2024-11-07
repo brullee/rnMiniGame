db401440690ac64a87c4fe9525645cedc88cf124
abdalla <abdalla.wohoush@gmail.com>
2024-11-07 23:04:37 +0300

    SplashScreen instead of AppLoading
    updated readme accordingly

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { useFonts } from "expo-font";
-import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
 
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
@@ -11,19 +11,18 @@ import GameOverScreen from "./screens/GameOverScreen";
 
 import Colors from "./src/constants/Colors";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
   const [userNum, setUserNum] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
   const [guessRounds, setGuessRounds] = useState(0);
+  const [appIsReady, setAppIsReady] = useState(false);
 
   const [fontsLoaded] = useFonts({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
 
   function pickedNumHandler(pickedNum) {
     setUserNum(pickedNum);
@@ -56,10 +55,38 @@ export default function App() {
     );
   }
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // await Font.loadAsync(Entypo.font);
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady && fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady, fontsLoaded]);
+
+  if (!appIsReady || !fontsLoaded) {
+    return null;
+  }
+
   return (
     <LinearGradient
       colors={[Colors.primary700, Colors.accent600]}
       style={styles.rootScreen}
+      onLayout={onLayoutRootView}
     >
       <ImageBackground
         source={require("./assets/images/background.png")}
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is a mini game I built using **React Native** and **Expo** where you pick a
 While building this game, I learned and revisited the following React Native concepts:
 
 - **Constants**: Using constant files (like `Colors.js`) to manage values such as colors, text sizes, or other static data
-- **Project Structure**: Organizing the files effectively for scalability
+- **Project Structure**: Organizing the files effectively
 - **Fonts**: Using `useFonts` from `expo-fonts` to load custom fonts
 - **Safe Area**: Implementing `SafeAreaView` and `SafeAreaProvider` from `react-native-safe-area-context` for proper display on all devices
 - **UI Components**: Learned how to utilize `ImageBackground`, `LinearGradient` from `expo-linear-gradient` for visually appealing backgrounds andor gradients
@@ -17,6 +17,8 @@ While building this game, I learned and revisited the following React Native con
 - **Data Handling**: Using `isNaN()` for number validation
 - **Icons**: Adding icons using `Ionicons`
 - **Side Effects**: Managing side effects with `useEffect`
+- **Performance Optimization**: Implementing `useCallback` to optimize functions that depend on specific state values, minimizing unnecessary re-renders
+- **Splash Screen**: Implementing a launch screen with `SplashScreen` from `expo-splash-screen` for a smooth startup experience
 - **List Rendering**: Revising `FlatList` and key handling for lists
 
 This project helped me reinforce key concepts in React Native and further improved my understanding of state management, and handling user interactions.
